d2ea167cc56bf18596e8583f710455f768ec44b0
Daniela Rojas Diaz <drojasd@unicesar.edu.co>
2026-03-01 21:05:07 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- src/sistemaacademicofinal/Estudiante.java
+++ src/sistemaacademicofinal/Estudiante.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistemaacademicofinal;
 
-/**
- *
- * @author DANIELA ROJAS
- */
 public class Estudiante {
+
+    String codigo;
+    String nombre;
+    String apellido;
+    int edad;
+    int semestre;
+    
+    public Estudiante() {
+    }
+
+    public Estudiante(String codigo, String nombre, String apellido, int edad, int semestre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.semestre = semestre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
     
 }
