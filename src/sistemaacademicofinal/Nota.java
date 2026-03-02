4a1f83b29f6b18c65d860968d94188c3d8894902
Daniela Rojas Diaz <drojasd@unicesar.edu.co>
2026-03-01 21:31:53 -0500

    feat: agregar encapsulamiento a clase Nota

--- src/sistemaacademicofinal/Nota.java
+++ src/sistemaacademicofinal/Nota.java
@@ -6,5 +6,49 @@ public class Nota {
     Asignatura asignatura;
     double valor;
     String periodo;
+
+    public Nota() {
+    }
+
+    public Nota(Estudiante estudiante, Asignatura asignatura, double valor, String periodo) {
+        this.estudiante = estudiante;
+        this.asignatura = asignatura;
+        this.valor = valor;
+        this.periodo = periodo;
+    }
+
+    public Asignatura getAsignatura() {
+        return asignatura;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setAsignatura(Asignatura asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
     
 }
